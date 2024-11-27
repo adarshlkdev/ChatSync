1ef467f0b0cdaa8c90dd2a2c32c971f960616897
Adarsh L K <erlkadarsh@gmail.com>
2024-11-27 13:35:27 +0530

    Remove console statements ⚡

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,13 +17,11 @@ function App() {
   const { authUser, checkAuth, isCheckingAuth , onlineUsers } = useAuthStore();
   const {theme} = useThemeStore();
 
-  console.log({onlineUsers});
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  console.log({ authUser });
 
   if (isCheckingAuth && !authUser)
     return (
